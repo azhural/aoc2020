ad296cb472e091d28872d1a43f6b69a0601ac4ef
Patrick Coenen <patrick@coenenmail.de>
2020-12-02 17:19:32 +0100

    init with basic input reader

---
+++ include/aoc2020/input/reader.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+
+namespace aoc {
+  namespace input {
+
+    class Reader {
+      public:
+        Reader(const std::string& file);
+
+        template<class T>
+          std::vector<T> get();
+
+      private:
+        std::ifstream fs;
+    };
+
+    template<class T>
+      std::vector<T> Reader::get() {
+        std::vector<T> v;
+        std::string s;
+        T value;
+        while(std::getline(fs, s)) {
+          std::stringstream iss{s};
+          iss >> value;
+          v.push_back(value);
+        }
+
+        return v;
+      }
+
+    template<>
+      inline std::vector<std::string> Reader::get<std::string>() {
+        std::vector<std::string> v;
+        std::string s;
+        while(std::getline(fs,s)) {
+          v.push_back(s);
+        }
+        return v;
+      }
+
+
+  }
+}
+
+
---
+++ src/input/reader.cpp
@@ -0,0 +1,19 @@
+#include<aoc2020/input/reader.h>
+#include<stdexcept>
+
+namespace aoc {
+  namespace input {
+
+    Reader::Reader(const std::string& file) {
+      fs.open(file);
+      if(!fs.is_open()) throw std::runtime_error("Unable to open file.");
+    }
+
+
+
+
+  
+  }
+}
+
+
